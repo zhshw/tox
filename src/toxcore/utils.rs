8ef89f75a2363696fae18968851ecd6230286359
Сухарик <suhr@i2pmail.org>
2018-11-30 02:43:19 +0300

    feat(utils): add `random_element`

--- src/toxcore/utils.rs
+++ src/toxcore/utils.rs
@@ -11,3 +11,12 @@ pub fn gen_ping_id() -> u64 {
     }
     ping_id
 }
+
+/// Get a reference to a random element of the slice
+pub fn random_element<T>(slice: &[T]) -> Option<&T> {
+    if slice.is_empty() { None }
+    else {
+        let n = random_usize() % slice.len();
+        Some(&slice[n])
+    }
+}
